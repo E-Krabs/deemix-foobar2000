89d0449f57394c888fe4de7dbb92e55e0ff8d997
Fast_and_Fuhrerious <Tyler.Smith11407@gmail.com>
2021-06-07 14:56:17 -0400

    Update strip.py

--- strip.py
+++ strip.py
@@ -1,6 +1,11 @@
-print('File name (foobar.txt):')
+import os
+
+print('Type File Name (foobar.txt):')
 file = input()
-with open(file) as infile, open('foobar.txt', 'w') as outfile:
+path = os.getcwd()
+name = path+'/'+file
+#print(name)
+with open(name) as infile, open('strip.txt', 'w') as outfile:
     for line in infile:
         if not line.strip(): continue  # skip the empty line
         outfile.write(line)  # non-empty line. Write it to output
