f75cec77b0d8f6410c0760bf2833551cdfc00460
Fast_and_Fuhrerious <Tyler.Smith11407@gmail.com>
2021-06-08 16:13:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,17 @@ count = 0
 for line in lines:
 	count += 1
 	unaccented_string = unidecode(line.strip())
-	encode = urllib.parse.quote(unaccented_string.strip()) #convert ASCII to Unicode
-	print(encode)
+	#print(unaccented_string)
+	encode = urllib.parse.quote(unaccented_string.strip()) #just encode artist/album (don't split)
+	artist_ascii, album_ascii = unaccented_string.split('/', 1) #split artist/album
+	artist_uni = urllib.parse.quote(artist_ascii.strip()) #convert ASCII to Unicode
+	album_uni = urllib.parse.quote(album_ascii.strip()) #convert ASCII to Unicode
 
-	content = urllib.request.urlopen('https://api.deezer.com/search/album/?q={}&index=0&limit=1&output=xml'.format(encode))
+	print(artist_uni)
+	print(album_uni)
+
+	content = urllib.request.urlopen('https://api.deezer.com/search/album/?q=artist:"{0}"%20album:"{1}"&index=0&limit=1&output=xml'.format(artist_uni, album_uni))
+
 	read_content = content.read()
 	soup = BeautifulSoup(read_content,'xml')
 
@@ -28,10 +35,26 @@ for line in lines:
 
 	for el in soup.select('root'):
 		total = int(el.total.text.strip())
-		#print(total)
+
 		if total == 0:
-			print('No Result For: {}'.format(line.strip()))
-			links.write('No Result For: {}'.format(line.strip()))
+			content = urllib.request.urlopen('https://api.deezer.com/search/album/?q={}&index=0&limit=1&output=xml'.format(encode))
+			read_content = content.read()
+			soup = BeautifulSoup(read_content,'xml')
+
+			for el in soup.select('root > data > album'):
+				link = el.link.text.strip()
+				title = el.title.text.strip()
+
+			for el in soup.select('root'):
+				total = int(el.total.text.strip())
+
+				if total == 0:
+					print('No Result For: {}'.format(line.strip()))
+					links.write('No Result For: {}'.format(line.strip()))
+				else:
+					print(link + ' - ' + title)
+					links.write(link + ' - ' + title)
+					links.write('\n')
 		else:
 			print(link + ' - ' + title)
 			links.write(link + ' - ' + title)
