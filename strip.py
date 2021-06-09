ac38912b9857bf48932fae98fb94bba3bbdad3b5
Fast_and_Fuhrerious <Tyler.Smith11407@gmail.com>
2021-06-09 19:30:46 -0400

    Update strip.py

--- strip.py
+++ strip.py
@@ -8,4 +8,4 @@ name = path+'/'+file
 with open(name) as infile, open('strip.txt', 'w') as outfile:
     for line in infile:
         if not line.strip(): continue  # skip the empty line
-        outfile.write(line)  # non-empty line. Write it to output
+        outfile.write(line.replace('"', '').replace(' - ', '/').replace('.flac', ''))  # non-empty line. Write it to output
